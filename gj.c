4b4150ea33d6f3cf9fedf22ddface5673a66ff4c
venkatroshan <35727735+venkatroshan@users.noreply.github.com>
2018-03-01 16:11:21 +0530

    Rename fg.c to gj.c

---
+++ gj.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{
+int a[10],i,j,temp,n;
+printf("enter  numbers");
+scanf("%d",&n);
+printf("enter numbers");
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+}
+for(i=0;i<n;i++)
+{
+for(j=i+1;j<n;j++)
+{
+if(a[i]>a[j])
+{
+temps=a[i];
+a[i]=a[j];
+a[j]=temp;
+}
+}
+}
+for(i=0;i<n;i++)
+{
+printf("%d",a[i]);
+}
+}
